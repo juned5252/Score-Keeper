6f093baee427152db86b170f5c02292195a00a00
Juned Alam <Juned Alam>
2016-11-20 21:20:41 -0500

    added mid-game reset handling/minor updates

--- ScoreKeeper.js
+++ ScoreKeeper.js
@@ -4,7 +4,9 @@ var resetButton = document.getElementById("reset");
 var numInput = document.querySelector("input")
 var p1display = document.getElementById("p1display");
 var p2display = document.getElementById("p2display");
+var winningScoreDisplay = document.querySelector("p span")
 var h1 = document.querySelector("h1");
+var p = document.querySelector("p");
 var p1score = 0;
 var p2score = 0;
 var gameOver = false;
@@ -33,6 +35,10 @@ p2Button.addEventListener("click", function(){
 });
 
 resetButton.addEventListener("click", function(){
+reset();
+});
+
+function reset(){
 p1score = 0;
 p2score = 0;
 p1display.textContent = 0;
@@ -40,4 +46,10 @@ p2display.textContent = 0;
 p1display.classList.remove("winner");
 p2display.classList.remove("winner");
 gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+winningScoreDisplay.textContent = this.value;
+winningScore = Number(this.value);
+reset();
 });
